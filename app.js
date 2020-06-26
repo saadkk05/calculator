4e2ed1c07040d2cf5de2a761d573e89e02c9aa03
saadkk05 <saadkk123321@gmail.com>
2020-06-27 01:55:06 +0500

    calculator

---
+++ app.js
@@ -0,0 +1,16 @@
+function aba(saa){
+var likhooo = document.getElementById("likhooo")
+likhooo.value += saa;
+}
+
+
+
+function chalbee(){
+    var likhooo = document.getElementById("likhooo")
+    likhooo.value = ""
+}
+
+function kiakehne(){
+    var likhooo = document.getElementById("likhooo")
+    likhooo.value = eval(likhooo.value)
+}
